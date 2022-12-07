785d7a862b5cdd9c4d5496ccd01c7a8a89224bc2
Stevanus Pangau <stevanuspangau50@gmail.com>
2022-12-07 14:18:36 +0700

    cek lapak

---
+++ lib/model/daftarBlok.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+class DaftarBlok {
+  final daftar;
+  final jmlBlok;
+
+  DaftarBlok({this.daftar, this.jmlBlok});
+
+  factory DaftarBlok.getDaftarBlok(Map<String, dynamic> object) {
+    int len = object['daftar'].length! ?? 0;
+    return DaftarBlok(daftar: object['daftar'], jmlBlok: len);
+  }
+
+  static Future<DaftarBlok> connectToAPI() async {
+    // String apiUrl = Uri.parse('http://localhost:8000/api/cek-saldo');
+    String apiUrl = "http://bumdes-sumowono.si-mantap.com/api/daftar-blok";
+
+    var apiResult =
+        await http.post(Uri.parse(apiUrl), body: {'koperasi_id': '1'});
+    var jsonObject = json.decode(apiResult.body);
+
+    if (apiResult.statusCode == 200) {
+      print('Berhasil API Daftar Blok');
+      print(apiResult.body);
+    } else {
+      print('Gagal APi Daftar Blok');
+    }
+    return DaftarBlok.getDaftarBlok(jsonObject);
+  }
+}

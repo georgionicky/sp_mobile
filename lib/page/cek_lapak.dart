01f497a3c28b61a09dda48c8e096ab52fe725605
Stevanus Pangau <stevanuspangau50@gmail.com>
2022-12-13 00:24:43 +0700

    cari lapak fix

--- lib/page/cek_lapak.dart
+++ lib/page/cek_lapak.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sp_mobile/beranda.dart';
 import 'package:sp_mobile/model/daftarBlok.dart';
 
+// ignore: camel_case_types
 class cek_Lapak extends StatefulWidget {
   const cek_Lapak({super.key});
 
@@ -10,25 +11,32 @@ class cek_Lapak extends StatefulWidget {
   State<cek_Lapak> createState() => _cek_LapakState();
 }
 
+// ignore: camel_case_types
 class _cek_LapakState extends State<cek_Lapak> {
+  // ignore: avoid_init_to_null
   late DaftarBlok? daftarBlok = null;
 
-  final _globalkey = GlobalKey<FormState>();
-  TextEditingController _noblok = TextEditingController();
+  bool search = false;
+  List list = [];
+
+  final TextEditingController _noblok = TextEditingController();
 
   getData() async {
     final SharedPreferences sharedPreferences =
         await SharedPreferences.getInstance();
+    // ignore: no_leading_underscores_for_local_identifiers
     var _token = sharedPreferences.getString('token');
 
     DaftarBlok.connectToAPI(_token!).then((value) {
       daftarBlok = value;
+      list.addAll(value?.daftar);
       setState(() {});
     });
   }
 
   @override
   void initState() {
+    // ignore: todo
     // TODO: implement initState
     super.initState();
     getData();
@@ -37,18 +45,22 @@ class _cek_LapakState extends State<cek_Lapak> {
   @override
   Widget build(BuildContext context) {
     int? len = daftarBlok?.jmlBlok ?? 0;
+
     return Scaffold(
+      // ignore: unnecessary_new
       appBar: new AppBar(
-        title: Text("Menu Cari Blok",
+        title: const Text("Menu Cari Blok",
             style: TextStyle(fontWeight: FontWeight.w700)),
         centerTitle: true,
+        // ignore: unnecessary_new
         leading: new IconButton(
-          icon: new Icon(Icons.arrow_back,
+          icon: const Icon(Icons.arrow_back,
               color: Color.fromARGB(255, 255, 255, 255)),
+          // ignore: unnecessary_new
           onPressed: () => Navigator.of(context).push(new MaterialPageRoute(
-              builder: (BuildContext context) => new beranda())),
+              builder: (BuildContext context) => const beranda())),
         ),
-        backgroundColor: Color.fromRGBO(39, 174, 96, 100),
+        backgroundColor: const Color.fromRGBO(39, 174, 96, 100),
       ),
       body: SingleChildScrollView(
         child: Container(
@@ -67,7 +79,7 @@ class _cek_LapakState extends State<cek_Lapak> {
                     mainAxisAlignment: MainAxisAlignment.end,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Center(
+                      const Center(
                         child: Text(
                           "Cari Blok",
                           style: TextStyle(
@@ -76,26 +88,26 @@ class _cek_LapakState extends State<cek_Lapak> {
                               fontWeight: FontWeight.bold),
                         ),
                       ),
-                      SizedBox(height: 10),
-                      Divider(
+                      const SizedBox(height: 10),
+                      const Divider(
                         color: Colors.black,
                       ),
-                      SizedBox(height: 10),
-                      Text(
+                      const SizedBox(height: 10),
+                      const Text(
                         "Nomor Blok",
                         style: TextStyle(
                             color: Colors.black,
                             fontSize: 16,
                             fontWeight: FontWeight.bold),
                       ),
-                      SizedBox(height: 5),
+                      const SizedBox(height: 5),
                       nomorBlok(),
-                      SizedBox(height: 30),
+                      const SizedBox(height: 30),
                     ],
                   ),
                 ),
               ),
-              Center(
+              const Center(
                 child: Text(
                   "Daftar Blok",
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
@@ -105,95 +117,108 @@ class _cek_LapakState extends State<cek_Lapak> {
                 flex: 5,
                 child: Container(
                   width: double.infinity,
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(70),
-                          topRight: Radius.circular(70))),
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20))),
                   child: Padding(
                     padding: const EdgeInsets.all(24.0),
-                    child: GridView.count(
+                    child: GridView.builder(
                       primary: false,
-                      padding: const EdgeInsets.all(4),
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10,
-                      crossAxisCount: 4,
-                      children: [
-                        for (var i = 0; i < len!; i++)
-                          Container(
-                            padding: const EdgeInsets.all(8),
-                            child: Center(
-                              child: Text(
-                                  '${daftarBlok?.daftar[i]['kode'] ?? "Kosong"}',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w700)),
-                            ),
-                            color: (daftarBlok?.daftar[i]['riwayat_retribusi']
-                                        .length ==
-                                    0)
-                                ? Colors.grey
-                                : (daftarBlok?.daftar[i]['riwayat_retribusi'][0]
-                                            ['status_kehadiran'] ==
-                                        "1")
-                                    ? Color.fromRGBO(39, 174, 96, 100)
-                                    : Color.fromARGB(156, 255, 2, 2),
+                      itemCount: (search == true) ? list.length : len,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: const EdgeInsets.all(8),
+                          // ignore: sort_child_properties_last
+                          child: Center(
+                            child: Text(
+                                (search == true)
+                                    ? '${list[index]['kode']}'
+                                    : '${daftarBlok?.daftar[index]['kode']}',
+                                textAlign: TextAlign.center,
+                                // ignore: prefer_const_constructors
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700)),
                           ),
-                      ],
+                          color: (search != true)
+                              ? (daftarBlok?.daftar[index]['riwayat_retribusi']
+                                          .length ==
+                                      0)
+                                  ? Colors.grey
+                                  : (daftarBlok?.daftar[index]
+                                                  ['riwayat_retribusi'][0]
+                                              ['status_kehadiran'] ==
+                                          "1")
+                                      ? const Color.fromRGBO(39, 174, 96, 100)
+                                      : const Color.fromARGB(156, 255, 2, 2)
+                              : (list[index]['riwayat_retribusi'].length == 0)
+                                  ? Colors.grey
+                                  : (list[index]['riwayat_retribusi'][0]
+                                              ['status_kehadiran'] ==
+                                          "1")
+                                      ? const Color.fromRGBO(39, 174, 96, 100)
+                                      : const Color.fromARGB(156, 255, 2, 2),
+                        );
+                      },
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        crossAxisCount: 5,
+                      ),
                     ),
                   ),
                 ),
-              ),
+              )
             ],
           ),
         ),
       ),
-      floatingActionButton: Container(
-        margin: EdgeInsets.only(bottom: 20),
-        child: SizedBox(
-          height: 50,
-          width: 300,
-          child: FloatingActionButton(
-            child: Center(
-                child: Container(
-              decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(10)),
-              child: Text(
-                "Cari",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-              ),
-            )), //child widget inside this button
-            shape:
-                BeveledRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            onPressed: () {
-              // if (_globalkey.currentState!.validate()) {
-              //   print("validate");
-              // }
-              //task to execute when this button is pressed
-            },
-            backgroundColor: Color.fromRGBO(39, 174, 96, 100),
-          ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+
+  _searchName(String name) {
+    if (name.isNotEmpty) {
+      setState(() {
+        list.clear();
+        //melakukan perulangan/looping
+        daftarBlok?.daftar.forEach((item) {
+          //jika list data ada yang mengandung susunan huruf yang dicari
+          //maka masukan ke dalam data list
+          if (item['kode'].toLowerCase().contains(name.toLowerCase())) {
+            list.add(item);
+          }
+        });
+      });
+    } else {
+      setState(() {
+        list.clear();
+        list.addAll(daftarBlok?.daftar);
+        search = false;
+      });
+    }
   }
 
   Widget nomorBlok() {
     return TextFormField(
-        obscureText: true,
         controller: _noblok,
         validator: (String? value) {
           if (value!.isEmpty) return "Nomor Blok Tidak Boleh Kosong";
           return null;
         },
-        style: TextStyle(fontSize: 12.0, height: 0.5),
-        decoration: InputDecoration(
+        style: const TextStyle(fontSize: 12.0, height: 0.5),
+        onChanged: ((value) {
+          if (value.isNotEmpty) {
+            search = true;
+          } else {
+            search = false;
+          }
+          _searchName(value);
+        }),
+        decoration: const InputDecoration(
             border: OutlineInputBorder(
                 borderSide: BorderSide(
               color: Color.fromRGBO(39, 174, 96, 100),
